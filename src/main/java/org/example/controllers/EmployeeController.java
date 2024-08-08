71550eb3bf0e536a32b90dffad466342041261e5
JohnnyWork718 <johnny----18@hotmail.com>
2024-08-08 16:46:26 +0100

    Linting changes 1

--- src/main/java/org/example/controllers/EmployeeController.java
+++ src/main/java/org/example/controllers/EmployeeController.java
@@ -2,8 +2,6 @@ package org.example.controllers;
 
 import io.swagger.annotations.Api;
 import org.example.services.EmployeeService;
-import org.example.services.TestService;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -15,15 +13,14 @@ import java.sql.SQLException;
 @Path("/api/employees") // change to /api/employee
 public class EmployeeController {
 
-    EmployeeService employeeService; // change to employee service when created
+    EmployeeService employeeService;
 
-    public EmployeeController(final EmployeeService employeeService) { // add employee service when setup
-        this.employeeService = employeeService; // change to employee service when created
+    public EmployeeController(final EmployeeService employeeService) {
+        this.employeeService = employeeService;
     }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response getAllSalesEmloyees() throws SQLException {
-        return Response.ok().entity(employeeService.getAllSalesEmloyees()).build(); //update to employee service when ready
-    }
-}
+        return Response.ok().entity(employeeService.getAllSalesEmloyees()).build();
+    };

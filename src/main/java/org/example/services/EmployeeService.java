fa17726bcf62a6086d1b3578165a0ebeae2804d6
John Hutchman <john_hutchman@hotmail.com>
2024-08-08 16:09:53 +0100

    more changes

--- src/main/java/org/example/services/EmployeeService.java
+++ src/main/java/org/example/services/EmployeeService.java
@@ -1,19 +1,20 @@
 package org.example.services;
 
 import org.example.daos.EmployeeDao;
-import org.example.daos.TestDao;
 import org.example.models.Employee;
 
 import java.sql.SQLException;
 import java.util.List;
 
-public class EmployeeService {
+public final class EmployeeService {
+    private EmployeeService(){
+    }
 
-    EmployeeDao employeeDao; // needs changed to employeeDAO when ready
+    EmployeeDao employeeDao;
 
     public EmployeeService(final EmployeeDao employeeDao) {
         this.employeeDao = employeeDao;
-    } // change to employeeDAO
+    }
 
 
     public List<Employee> getAllSalesEmloyees() throws SQLException {
